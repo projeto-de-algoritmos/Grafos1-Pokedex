a24f7b3f20001e50914ea06f6faf9c33864fb738
F1reFinger <180121847@aluno.unb.br>
2023-04-27 20:49:03 -0300

    main and dictionary created

--- testgraph.py
+++ testgraph.py
@@ -1,3 +1,19 @@
+from collections import defaultdict
+
+
+class Pokedex:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_pokemon(self, name, evolution):
+        self.graph[name].append(evolution)
+
+    def show_pokedex(self):
+        print("Pokedex:")
+        for pokemon in self.graph.keys():
+            evolutions = ", ".join(self.graph[pokemon])
+            print(f"{pokemon}: {evolutions}")
+
 f = open("testador.txt", "a")
 
 def bfs(self, start, end):
@@ -27,7 +43,7 @@ def bfs(self, start, end):
     print(f"Nenhum caminho de {start} para {end} encontrado.")
 
 
-def get_Data():
+'''def get_Data():
     i = 0
     a = input('pokemon name ')
     print('seu pokemon se chama :', a , 'esta correto?')
@@ -42,10 +58,59 @@ def get_Data():
             i = 0
     b = input('Qual o nome da evolução dele?')
     c = "->"
-    f.writelines([a, c, b, "\n"])
+    f.writelines([a, c, b, "\n"])'''
 
-get_Data()
-with open('testador.txt') as f:
-    lines = f.readlines()
-print(lines)
-f.close()
+if __name__ == "__main__":
+    pokedex = Pokedex()
+    # Load pokedex from file
+    try:
+        with open("testador.txt", "r") as f:
+            for line in f:
+                name, *evolutions = line.strip().split()
+                for evolution in evolutions:
+                    pokedex.add_pokemon(name, evolution)
+    except FileNotFoundError:
+        pass
+
+    while True:
+        print("\nMenu:")
+        print("1. Add pokemon")
+        print("2. Show pokedex")
+        print("3. Save pokedex")
+        print("4. DFS search")
+        print("5. BFS search")
+        print("6. Quit")
+        choice = input("Enter choice (1-6): ")
+
+        if choice == "1":
+            name = input("Enter pokemon name: ")
+            evolution = input("Enter evolution: ")
+            pokedex.add_pokemon(name, evolution)
+            print(f"{name} added to pokedex.")
+
+        elif choice == "2":
+            pokedex.show_pokedex()
+
+        elif choice == "3":
+            with open("pokedex.txt", "w") as f:
+                for pokemon, evolutions in pokedex.graph.items():
+                    f.write(f"{pokemon} {' '.join(evolutions)}\n")
+            print("Pokedex saved to file.")
+
+        elif choice == "4":
+            print("function under construction!!!")
+
+        elif choice == "5":
+            name = input("Enter pokemon name to search: ")
+            try:
+                print(f"BFS search starting from {name}:")
+                pokedex.bfs("Arceus", name)
+            except KeyError:
+                print(f"{name} not found in pokedex.")
+
+        elif choice == "6":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid choice. Try again.")
